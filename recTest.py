53b3b71423a17cf881e27c5862f76abc81dd7ab2
nmlachmayr <61168496+nmlachmayr@users.noreply.github.com>
2021-12-04 15:48:11 -0700

    Add files via upload

--- recTest.py
+++ recTest.py
@@ -5,6 +5,7 @@ import json
 import csv
 import time
 from scipy import stats
+from scipy.spatial import distance
 
 #from lightfm import LightFM
 
@@ -40,14 +41,14 @@ def createDicOfUsers(users):
     return d
 
 
-def calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, user):
+def calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, user, N):
 
     temp1 = [0] * len(gamesDic)
     temp1[0] = float(listOfUserID.index(user))
     for y in dicofusers[user]:
         temp1[listOfGameID.index(y[0])+1] = 1
     data = []
-    for x in listOfUserID[:100]:
+    for x in listOfUserID[:N]:
         temp2 = [0] * len(gamesDic)
         temp2[0] = float(listOfUserID.index(x))
         for y in dicofusers[x]:
@@ -57,19 +58,63 @@ def calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, user):
         #print(x)
         #print(t1,t2)
         data.append((x, t1))
+    data.sort(key=lambda x:x[1])
+    return data#listofSimilarUsers
+
+def calcCosine(dicofusers, gamesDic, listOfUserID, listOfGameID, user, N):
     
-    return data
+    temp1 = [0] * len(gamesDic)
+    temp1[0] = float(listOfUserID.index(user))
+    for y in dicofusers[user]:
+        temp1[listOfGameID.index(y[0])+1] = 1
+    data = []
+    for x in listOfUserID[:N]:
+        temp2 = [0] * len(gamesDic)
+        temp2[0] = float(listOfUserID.index(x))
+        for y in dicofusers[x]:
+            temp2[listOfGameID.index(y[0])+1] = 1
+
+        t1 = distance.cosine(temp1, temp2)
+        data.append((x, t1))
+    data.sort(key=lambda x:x[1])
+    return data#listofSimilarUsers
+
+def calcJaccard(dicofusers, gamesDic, listOfUserID, listOfGameID, user, N):
+    
+    temp1 = [0] * len(gamesDic)
+    temp1[0] = float(listOfUserID.index(user))
+    for y in dicofusers[user]:
+        temp1[listOfGameID.index(y[0])+1] = 1
+    data = []
+    for x in listOfUserID[:N]:
+        temp2 = [0] * len(gamesDic)
+        temp2[0] = float(listOfUserID.index(x))
+        for y in dicofusers[x]:
+            temp2[listOfGameID.index(y[0])+1] = 1
+
+        t1 = distance.jaccard(temp1, temp2)
+        data.append((x, t1))
+    data.sort(key=lambda x:x[1])
+    return data#listofSimilarUsers
 
 def getRecs(listOfsimilarUsers, dicofUsers, gamesDic):
     Recgames = []
     for x in listOfsimilarUsers:
+        #print(x)
         for y in dicofUsers[x[0]]:
+            #print(y)
             Recgames.append((y[0], x[1] * float(y[1])))
     Recgames.sort(key=lambda x:x[1])
-    print(Recgames)
+    
     return Recgames
 
+def checkResults(recs, expectedrecs):
+    c = 0;
+    for x in recs:
+        if x[0] in expectedrecs:
+            c += 1
 
+    return c
 
 def main():
     gamesDic = readJSON('meta_Video_Games.json')
@@ -87,19 +132,35 @@ def main():
     for x in dicofusers:
         listOfUserID.append(x)
     listOfUserID.sort()
-    print("pearson testing")
-    data = calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ')#currently outputs Pval correctly but there is not much difference between the users
-    print("done")
-    data.sort(key=lambda x:x[1])
+
     
-    getRecs(data[-5:], dicofusers, gamesDic)
+    data = calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)#currently outputs Pval correctly but there is not much difference between the users
+    data2 = calcCosine(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)
+    data3 = calcJaccard(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)
+
+    d = getRecs(data[-500:], dicofusers, gamesDic)
+    d2 = getRecs(data2[-500:], dicofusers, gamesDic)
+    d3 = getRecs(data3[-500:], dicofusers, gamesDic)
+    
 
 
-    #for x in userList:
-    #    templ = [0] * len(gamesDic)
+    l = []
+    for x in dicofusers['ARHP7M2HVVFLZ']:
+        #print(len(gamesDic[x[0]]['related']['also_bought']))
+        for y in gamesDic[x[0]]['related']['also_bought']:
+            l.append(y)
+        for y in gamesDic[x[0]]['related']['buy_after_viewing']:
+            l.append(y)
+        for y in gamesDic[x[0]]['related']['bought_together']:
+            l.append(y)
 
+    c = checkResults(d, l)
+    c2 = checkResults(d2, l)
+    c3 = checkResults(d3, l)
 
-
+    print("pearson: ", c)
+    print("cosine: ", c2)
+    print("jaccard: ", c3)
     return 0
 
 if __name__ == '__main__':
