2f40917469729194ad11051d72c38a0f5c091b5f
nmlachmayr <61168496+nmlachmayr@users.noreply.github.com>
2021-12-06 18:19:31 -0700

    Add files via upload

--- recTest.py
+++ recTest.py
@@ -55,6 +55,7 @@ def calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, user, N):
             temp2[listOfGameID.index(y[0])+1] = 1
     
         t1, t2 = stats.pearsonr(temp1, temp2)
+        
         #print(x)
         #print(t1,t2)
         data.append((x, t1))
@@ -97,6 +98,30 @@ def calcJaccard(dicofusers, gamesDic, listOfUserID, listOfGameID, user, N):
     data.sort(key=lambda x:x[1])
     return data#listofSimilarUsers
 
+def calcAllThree(dicofusers, gamesDic, listOfUserID, listOfGameID, user, N):
+    
+    temp1 = [0] * len(gamesDic)
+    temp1[0] = float(listOfUserID.index(user))
+    for y in dicofusers[user]:
+        temp1[listOfGameID.index(y[0])+1] = 1
+    data = []
+    for x in listOfUserID[:N]:
+        temp2 = [0] * len(gamesDic)
+        temp2[0] = float(listOfUserID.index(x))
+        for y in dicofusers[x]:
+            temp2[listOfGameID.index(y[0])+1] = 1
+    
+        t1, t2 = stats.pearsonr(temp1[1:], temp2[1:])
+        t3 = distance.cosine(temp1[1:], temp2[1:])
+        t4 = distance.jaccard(temp1[1:], temp2[1:])    
+
+        t5 = t1 * t3 * t4
+        #print(x)
+        #print(t1,t2)
+        data.append((x, t5))
+    data.sort(key=lambda x:x[1])
+    return data#listofSimilarUsers
+
 def getRecs(listOfsimilarUsers, dicofUsers, gamesDic):
     Recgames = []
     for x in listOfsimilarUsers:
@@ -116,6 +141,19 @@ def checkResults(recs, expectedrecs):
 
     return c
 
+def createReleventList(user, dicofusers, gamesDic):
+    l = []
+    for x in dicofusers[user]:
+        #print(len(gamesDic[x[0]]['related']['also_bought']))
+        for y in gamesDic[x[0]]['related']['also_bought']:
+            l.append(y)
+        for y in gamesDic[x[0]]['related']['buy_after_viewing']:
+            l.append(y)
+        for y in gamesDic[x[0]]['related']['bought_together']:
+            l.append(y)
+
+    return l
+
 def main():
     gamesDic = readJSON('meta_Video_Games.json')
     userList = readCSV('ratings_Video_Games.csv')
@@ -133,34 +171,32 @@ def main():
         listOfUserID.append(x)
     listOfUserID.sort()
 
+
     
-    data = calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)#currently outputs Pval correctly but there is not much difference between the users
-    data2 = calcCosine(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)
-    data3 = calcJaccard(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)
+    data = calcPearson(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 1000)#currently outputs Pval correctly but there is not much difference between the users
+    #data2 = calcCosine(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)
+    #data3 = calcJaccard(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 200000)
+    data4 = calcAllThree(dicofusers, gamesDic, listOfUserID, listOfGameID, 'ARHP7M2HVVFLZ', 100000)
 
     d = getRecs(data[-500:], dicofusers, gamesDic)
-    d2 = getRecs(data2[-500:], dicofusers, gamesDic)
-    d3 = getRecs(data3[-500:], dicofusers, gamesDic)
+    #d2 = getRecs(data2[-500:], dicofusers, gamesDic)
+    #d3 = getRecs(data3[-500:], dicofusers, gamesDic)
+    d4 = getRecs(data4[-500:], dicofusers, gamesDic)
     
 
+    l = createReleventList( 'ARHP7M2HVVFLZ', dicofusers, gamesDic)
 
-    l = []
-    for x in dicofusers['ARHP7M2HVVFLZ']:
-        #print(len(gamesDic[x[0]]['related']['also_bought']))
-        for y in gamesDic[x[0]]['related']['also_bought']:
-            l.append(y)
-        for y in gamesDic[x[0]]['related']['buy_after_viewing']:
-            l.append(y)
-        for y in gamesDic[x[0]]['related']['bought_together']:
-            l.append(y)
+    
 
     c = checkResults(d, l)
-    c2 = checkResults(d2, l)
-    c3 = checkResults(d3, l)
+    
+    #c2 = checkResults(d2, l)
+    #c3 = checkResults(d3, l)
 
-    print("pearson: ", c)
-    print("cosine: ", c2)
-    print("jaccard: ", c3)
+    print("pearson: ", c, len(l))
+    #print("cosine: ", c2)
+    #print("jaccard: ", c3)
+    print("results on all three: ", checkResults(d4, l), len(l))
     return 0
 
 if __name__ == '__main__':
